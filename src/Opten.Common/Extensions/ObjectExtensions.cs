05a849539673fdae483f2679eca77968c6774c6f
Calvin Frei <calvin.frei@opten.ch>
2018-08-22 15:00:55 +0200

    Adding some extensions

---
+++ src/Opten.Common/Extensions/ObjectExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml;
+
+namespace Opten.Common.Extensions
+{
+	/// <summary>
+	/// The Object Extensions.
+	/// </summary>
+	public static class ObjectExtensions
+	{
+
+		/// <summary>
+		/// Converts a value to a XML string.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="type">The type.</param>
+		/// <returns></returns>
+		/// <exception cref="NotSupportedException">Cannot convert type " + type.FullName + " to a string using ToXmlString as it is not supported by XmlConvert</exception>
+		public static string ToXmlString(this object value, Type type)
+		{
+			if (value == null) return string.Empty;
+			if (type == typeof(string)) return (string.IsNullOrWhiteSpace(value.ToString()) ? "" : value.ToString());
+			if (type == typeof(bool)) return XmlConvert.ToString((bool)value);
+			if (type == typeof(byte)) return XmlConvert.ToString((byte)value);
+			if (type == typeof(char)) return XmlConvert.ToString((char)value);
+			if (type == typeof(DateTime)) return XmlConvert.ToString((DateTime)value, XmlDateTimeSerializationMode.Unspecified);
+			if (type == typeof(DateTimeOffset)) return XmlConvert.ToString((DateTimeOffset)value);
+			if (type == typeof(decimal)) return XmlConvert.ToString((decimal)value);
+			if (type == typeof(double)) return XmlConvert.ToString((double)value);
+			if (type == typeof(float)) return XmlConvert.ToString((float)value);
+			if (type == typeof(Guid)) return XmlConvert.ToString((Guid)value);
+			if (type == typeof(int)) return XmlConvert.ToString((int)value);
+			if (type == typeof(long)) return XmlConvert.ToString((long)value);
+			if (type == typeof(sbyte)) return XmlConvert.ToString((sbyte)value);
+			if (type == typeof(short)) return XmlConvert.ToString((short)value);
+			if (type == typeof(TimeSpan)) return XmlConvert.ToString((TimeSpan)value);
+			if (type == typeof(bool)) return XmlConvert.ToString((bool)value);
+			if (type == typeof(uint)) return XmlConvert.ToString((uint)value);
+			if (type == typeof(ulong)) return XmlConvert.ToString((ulong)value);
+			if (type == typeof(ushort)) return XmlConvert.ToString((ushort)value);
+
+			throw new NotSupportedException("Cannot convert type " + type.FullName + " to a string using ToXmlString as it is not supported by XmlConvert");
+		}
+
+	}
+}
